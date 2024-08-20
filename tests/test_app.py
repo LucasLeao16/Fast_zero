05fa9237b9d41ed91a804777d853d33cb6851e88
LucasLeao16 <lucas99br@gmail.com>
2024-08-20 19:53:01 -0300

    Adicionado autenticação JWT inicial

--- tests/test_app.py
+++ tests/test_app.py
@@ -26,9 +26,58 @@ def test_read_users(client):
     assert response.status_code == HTTPStatus.OK
 
 
-def test_update_user(client):
-    pass
+def test_update_user(client, user, token):
+    response = client.put(
+        f"/users/{user.id}",
+        headers={"Authorization": f"Bearer {token}"},
+        json={
+            "username": "bob",
+            "email": "bob@example.com",
+            "password": "mynewpassword",
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "username": "bob",
+        "email": "bob@example.com",
+        "id": user.id,
+    }
 
 
-def test_delete_user(client):
-    pass
+def test_update_user_not_exist(client, user):
+    response = client.put(
+        "/users/0",
+        json={
+            "username": "bob",
+            "email": "bob@example.com",
+            "password": "mynewpassword",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+
+
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f"/users/{user.id}",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"message": "User deleted"}
+
+
+def test_delete_user_not_exist(client, user):
+    response = client.delete("/users/0")
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+
+
+def test_get_token(client, user):
+    response = client.post(
+        "/token",
+        data={"username": user.email, "password": user.clean_password},
+    )
+    token = response.json()
+
+    assert response.status_code == HTTPStatus.OK
+    assert "access_token" in token
+    assert "token_type" in token
